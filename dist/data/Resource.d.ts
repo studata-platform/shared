dd76ae698f9bd15c8ec8b7aa7de9f7379a1e85eb
Leroy Korterink <lkorterink@outlook.com>
2020-01-06 20:17:24 +0100

    Update build

---
+++ dist/data/Resource.d.ts
@@ -0,0 +1,13 @@
+declare enum Resource {
+    Activity = "Activity",
+    Article = "Article",
+    Event = "Event",
+    Organization = "Organization",
+    Project = "Project",
+    ProjectState = "ProjectState",
+    QuickscanQuestion = "QuickscanQuestion",
+    QuickscanResult = "QuickscanResult",
+    User = "User"
+}
+export default Resource;
+//# sourceMappingURL=Resource.d.ts.map
---
+++ dist/data/Resource.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Resource;
+(function (Resource) {
+    Resource["Activity"] = "Activity";
+    Resource["Article"] = "Article";
+    Resource["Event"] = "Event";
+    Resource["Organization"] = "Organization";
+    Resource["Project"] = "Project";
+    Resource["ProjectState"] = "ProjectState";
+    Resource["QuickscanQuestion"] = "QuickscanQuestion";
+    Resource["QuickscanResult"] = "QuickscanResult";
+    Resource["User"] = "User";
+})(Resource || (Resource = {}));
+exports.default = Resource;
